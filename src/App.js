f1a39c62cfafa9438a69c3d6241f1c8f3c68a813
anjaneyulu36 <anjiantholla@gmail.com>
2024-01-30 12:07:34 +0530

    adc

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Anji from './anji';
 import { startTransition } from 'react';
-import Anji from './anji';
+
 function App() {
   return (<div>
   
